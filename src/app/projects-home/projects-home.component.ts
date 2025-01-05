f161c94fa88b42dff181f393a0be0f8fb2478733
3y3l3ss0ur0b0r0s <danielle.bertulfo.coding@gmail.com>
2025-01-04 22:52:06 -0500

    Add binding and styling code

--- src/app/projects-home/projects-home.component.ts
+++ src/app/projects-home/projects-home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-projects-home',
@@ -7,6 +7,31 @@ import { Component } from '@angular/core';
   templateUrl: './projects-home.component.html',
   styleUrl: './projects-home.component.css'
 })
-export class ProjectsHomeComponent {
+export class ProjectsHomeComponent implements OnInit {
+  firstName: string = "firstName";
+  lastName: string = "lastName";
+  phoneNumber: string = "phoneNumber";
 
+  setProperty(event: Event) {
+    let target = event.target as HTMLInputElement;
+    let property: string = target.id;
+    switch(property) {
+      case "firstNameInput":
+        this.firstName = target.value;
+        break;
+      case "lastNameInput":
+        this.lastName = target.value;
+        break;
+      case "phoneNumberInput":
+        this.phoneNumber = target.value;
+        break;
+      default:
+        console.warn("No matching ID found for input.");
+        break;
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit(): void { }
 }
