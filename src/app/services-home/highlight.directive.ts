6627d0e1f93e27fbf7c3cd8afb8f690f326e52eb
3y3l3ss0ur0b0r0s <danielle.bertulfo.coding@gmail.com>
2025-01-05 17:02:17 -0500

    Add directive code

--- src/app/services-home/highlight.directive.ts
+++ src/app/services-home/highlight.directive.ts
@@ -1,14 +1,34 @@
-import { Directive, ElementRef } from '@angular/core';
-
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 @Directive({
-  selector: '[appHighlight]',
-  standalone: true
+  standalone: true,
+  selector: '[appHighlight]'
 })
+
+export class HighlightDirective {
+  @Input() appHighlight = ''
+    
+  constructor(private el: ElementRef) {}
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('white');
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+}
+
+/* 
+Code for debugging section:
 export class HighlightDirective {
   constructor(el: ElementRef) {
-    //debugger;
-    //el.nativeElement.style.backgroundColor = 'purple';
-    /*
+    // Debugging with debugger statement
+    debugger;
+    el.nativeElement.style.backgroundColor = 'purple';
+    // Debugging with console
     try {
       console.log(el);
       el.nativeElement.style.backgroundColor = 'purple';
@@ -16,6 +36,6 @@ export class HighlightDirective {
     } catch (e) {
       console.error(e);
     }
-    */
   }
 }
+*/
